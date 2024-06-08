07e0f7df5d3790bdf7a7100de4966baa6f3c6a74
RokiTino <40834566+RokiTino@users.noreply.github.com>
2024-06-08 10:47:06 +0200

    creating chat screen

--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -1,14 +1,125 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {  View, Text, TextInput, FlatList, Image, TouchableOpacity, Platform } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import Matches from "../../components/UI/matches";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import {
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { chatData } from "../content/index";
+import { useNavigation } from "@react-navigation/native";
+
+const android = Platform.OS === "android";
 
 const ChatScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
+    <SafeAreaView
+      className=""
+      style={{paddingTop: android ? hp(3): 0}}>
+        <View className="px-4">
+          <Text className="uppercase font-semibold text-neutral-500 tracking-wider">Matches</Text>
+        </View>
+        <Matches />
+
+        <View className="mx-4 my-6 flex-row items-center bg-neutral-200 px-3 py-4 rounded-2xl">
+        <TextInput
+          placeholder="Search"
+          placeholderTextColor={"gray"}
+          style={{
+            fontSize: hp(1.7),
+          }}
+          className="flex-1 text-base mb-1 pl-1 tracking-widest"
+        />
+        <View className="">
+          <MagnifyingGlassIcon size={hp(2.5)} color={"gray"} strokeWidth={3} className="flex-1 justify-end"/>
+        </View>
+        </View>
+
+        <View className="px-4">
+        <View className="border-b border-neutral-300 py-4">
+          <Text className="uppercase font-semibold text-neutral-500 tracking-wider ">
+            CHAT
+          </Text>
+        </View>
+
+        <FlatList
+          data={chatData}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              className="w-full py-3 items-center flex-row border-b  border-neutral-300"
+              onPress={() =>
+                navigation.navigate("ChatDetails", {
+                  chat: item.chat,
+                  imgUrl: item.imgUrl,
+                  name: item.name,
+                  age: item.age,
+                })
+              }
+            >
+              {/* Avatar */}
+              <View
+                className="w-[17%] justify-center"
+                style={{
+                  width: hp(7),
+                  height: hp(7),
+                }}
+              >
+                <Image
+                  source={item.imgUrl}
+                  style={{
+                    width: "90%",
+                    height: "90%",
+                  }}
+                  className="rounded-full"
+                />
+              </View>
+
+              {/* Information */}
+              <View
+                className="w-[82%]"
+                style={{
+                  height: hp(6),
+                }}
+              >
+                <View className="flex-row justify-between items-center">
+                  <View className="flex-row justify-center">
+                    <View className="flex-row">
+                      <Text className="font-bold text-base  ">
+                        {item.name} {", "}
+                      </Text>
+                      <Text className="font-bold text-base mr-1">
+                        {item.age}
+                      </Text>
+                    </View>
+                    {item.isOnline && (
+                      <View className=" justify-center items-center">
+                        <View className="w-2 h-2 bg-[#F26322] rounded-full ml-1 justify-center items-center"></View>
+                      </View>
+                    )}
+                  </View>
+                  <Text className="text-sm tracking-tight">
+                    {item.timeSent}
+                  </Text>
+                </View>
+                <View>
+                  <Text className="font-semibold text-xs text-neutral-500">
+                    {item.lastMessage.length > 45
+                      ? item.lastMessage.slice(0, 45) + "..."
+                      : item.lastMessage}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+        </View>
+
+      </SafeAreaView>
+  );
 }
 
 export default ChatScreen
-
-const styles = StyleSheet.create({})
